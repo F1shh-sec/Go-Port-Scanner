033d23d3f89fbd5e9e024df1c732cc8d3dfefd20
George <Linkeydoo@gmail.com>
2022-01-26 12:53:59 -0500

    Quality of life changes

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Go-Port-Scanner
-small and super quick concurrent port scanner using the GO language
-Not stealthy at all. 
-The udp port scanner doesnt work too well because it only checks if it can connect. The TCP works well
+**small and super quick concurrent port scanner using the GO language**
+
+This scanner is not stealthy at all. it generates a massive amount of traffic. 
+
+The UDP port scanner doesn't work too well because it only checks if it can connect. 
+The TCP scanner works without a flaw.
 
 # USAGE
-To run the command from the terminal, use the following syntax
-> go run main.go protocol target_ip Starting_port Ending_port
+To run the command from the terminal, use the following syntax:
+> go run main.go target_ip Starting_port Ending_port protocol
 
-Example:
-> go run main.go tcp 192.168.0.1 1 2000
+If the protocol is not included, it will default to TCP:
+
+**Example:**
+> go run main.go 192.168.0.1 25564 25566
+
+By default, closed ports are hidden. If you want to see closed ports, You can uncomment the lines in the connect function
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 /**
 Author: F1shh-sec
 SOURCE: https://github.com/F1shh-sec/Go-Port-Scanner
-USAGE go run main.go protocol target_ip Starting_port Ending_port
+USAGE go run main.go target_ip Starting_port Ending_port protocol
 */
 
 func main() {
@@ -22,38 +22,38 @@ func main() {
 		// if there are no arguments provided
 		noArgs()
 	} else if argsnum >= 2 {
-		// USAGE go run main.go protocol target_ip Starting_port Ending_port
+		// USAGE go run main.go target_ip Starting_port Ending_port protocol
 		withArgs(argsnum)
 	}
 
 }
 func withArgs(argsnum int) {
 	// if there are
-	prot := os.Args[1]
-	ipa := os.Args[2]
-	startport, _ := strconv.Atoi(os.Args[3])
-	endport, _ := strconv.Atoi(os.Args[4])
-	
+
+	ipa := os.Args[1]
+	startport, _ := strconv.Atoi(os.Args[2])
+	endport, _ := strconv.Atoi(os.Args[3])
+
+	var prot string
+	if argsnum == 5 {
+		prot = os.Args[4]
+	} else {
+		prot = "tcp"
+	}
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	fmt.Println("Protocol:", prot)
+	fmt.Println("Target Ip:", ipa)
+	fmt.Println("Starting Port:", startport)
+	fmt.Println("End Port:", endport)
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 	runScan(prot, ipa, startport, endport)
 
-}
-func runScan(prot string, ipa string, startport int, endport int) {
-	var wg sync.WaitGroup
-
-	if endport > startport {
-		for i := startport; i <= endport; i++ {
-			wg.Add(1)
-			go connect(ipa, prot, i, &wg)
-		}
-	} else {
-		fmt.Println("You have a larger starting port then end port...")
-	}
-	wg.Wait()
 }
 func noArgs() {
 	var ipa, prot string
 	var startport, endport int
 	// Gets the protocol
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 	fmt.Print("Enter a Protocol: ")
 	fmt.Scanf("%s\n", &prot)
 	prot = strings.Replace(prot, "\n", "", -1)
@@ -70,8 +70,23 @@ func noArgs() {
 	// grabs ending port
 	fmt.Print("Enter a ending port: ")
 	fmt.Scanf("%d\n", &endport)
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 
 	runScan(prot, ipa, startport, endport)
+}
+
+func runScan(prot string, ipa string, startport int, endport int) {
+	var wg sync.WaitGroup
+
+	if endport > startport {
+		for i := startport; i <= endport; i++ {
+			wg.Add(1)
+			go connect(ipa, prot, i, &wg)
+		}
+	} else {
+		fmt.Println("You have a larger starting port then end port...")
+	}
+	wg.Wait()
 }
 
 /**
@@ -87,13 +102,14 @@ func connect(ipaddr string, prot string, port int, wg *sync.WaitGroup) {
 	// If we can not connect to the port
 
 	if err != nil {
-		if prot == "tcp" {
-			//tpc
-			fmt.Println(port, ": closed/filtered")
-		} else {
-			//udp
-			fmt.Println(port, ": closed/filtered")
-		}
+		//Uncomment Below Lines to show closed ports
+		//if prot == "tcp" {
+		//	//tpc
+		//	fmt.Println(port, ": closed/filtered")
+		//} else {
+		//	//udp
+		//	fmt.Println(port, ": closed/filtered")
+		//}
 
 	} else {
 		// If we can Connect to the port
