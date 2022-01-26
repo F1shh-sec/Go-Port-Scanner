c4ec0b3fe067fc793258993a16bcc4785752cb0e
F1shh <Linkeydoo@gmail.com>
2022-01-26 11:45:02 -0500

    Updated to add command line support

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Go-Port-Scanner
 small and super quick concurrent port scanner using the GO language
-Not stealthy at all. Scans for TCP poarts but you can easily change that 
+Not stealthy at all. 
+The udp port scanner doesnt work too well because it only checks if it can connect. The TCP works well
+
+# USAGE
+To run the command from the terminal, use the following syntax
+> go run main.go protocol target_ip Starting_port Ending_port
+
+Example:
+> go run main.go tcp 192.168.0.1 1 2000
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -11,54 +12,100 @@ import (
 
 /**
 Author: F1shh-sec
-
+SOURCE: https://github.com/F1shh-sec/Go-Port-Scanner
+USAGE go run main.go protocol target_ip Starting_port Ending_port
 */
 
 func main() {
-	var ipa string
-	var startport, endport int
+	argsnum := len(os.Args)
+	if argsnum < 2 {
+		// if there are no arguments provided
+		noArgs()
+	} else if argsnum == 5 {
+		// USAGE go run main.go protocol target_ip Starting_port Ending_port
+		withArgs()
+	}
 
-	// Gets input Ip address
-	fmt.Println("Enter a host IP: ")
-	fmt.Scanf("%s\n", &ipa)
-	ipa = strings.Replace(ipa, "\n", "", -1)
+}
+func withArgs() {
+	// if there are
+	prot := os.Args[1]
+	ipa := os.Args[2]
+	startport, _ := strconv.Atoi(os.Args[3])
+	endport, _ := strconv.Atoi(os.Args[4])
 
-	// Grabs starting port
-	fmt.Println("Enter a starting port: ")
-	fmt.Scanf("%d\n", &startport)
+	runScan(prot, ipa, startport, endport)
 
-	// grabs ending port
-	fmt.Println("Enter a ending port: ")
-	fmt.Scanf("%d\n", &endport)
-
+}
+func runScan(prot string, ipa string, startport int, endport int) {
 	var wg sync.WaitGroup
 
 	if endport > startport {
 		for i := startport; i <= endport; i++ {
 			wg.Add(1)
-			go connect(ipa, i, &wg)
+			go connect(ipa, prot, i, &wg)
 		}
+	} else {
+		fmt.Println("You have a larger starting port then end port...")
 	}
 	wg.Wait()
+}
+func noArgs() {
+	var ipa, prot string
+	var startport, endport int
+	// Gets the protocol
+	fmt.Print("Enter a Protocol: ")
+	fmt.Scanf("%s\n", &prot)
+	prot = strings.Replace(prot, "\n", "", -1)
+
+	// Gets input Ip address
+	fmt.Print("Enter a host IP: ")
+	fmt.Scanf("%s\n", &ipa)
+	ipa = strings.Replace(ipa, "\n", "", -1)
+
+	// Grabs starting port
+	fmt.Print("Enter a starting port: ")
+	fmt.Scanf("%d\n", &startport)
+
+	// grabs ending port
+	fmt.Print("Enter a ending port: ")
+	fmt.Scanf("%d\n", &endport)
+
+	runScan(prot, ipa, startport, endport)
 }
 
 /**
 Attempts to connect to a given port.
 */
-func connect(ipaddr string, port int, wg *sync.WaitGroup) {
+func connect(ipaddr string, prot string, port int, wg *sync.WaitGroup) {
 	// Converts the int to a string
 	portstr := strconv.Itoa(port)
 	// Connects the port number and the ip address
 	ipandport := ipaddr + ":" + portstr
 	// Attempts to connect to the port.
-	conn, err := net.DialTimeout("tcp", ipandport, 1*time.Second)
+	conn, err := net.DialTimeout(prot, ipandport, 1*time.Second)
 	// If we can not connect to the port
+
 	if err != nil {
-		fmt.Println(port, ": closed")
+		if prot == "tcp" {
+			//tpc
+			fmt.Println(port, ": closed/filtered")
+		} else {
+			//udp
+			fmt.Println(port, ": closed/filtered")
+		}
+
 	} else {
 		// If we can Connect to the port
 		conn.Close()
-		fmt.Println(port, ": open")
+		if prot == "tcp" {
+			//tcp
+			fmt.Println(port, ": open")
+		} else {
+			//udp
+			fmt.Println(port, ": open/Filtered")
+		}
+
 	}
 	defer wg.Done()
 }
